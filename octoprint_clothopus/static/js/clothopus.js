b90e70ee30fb5c5bfd880295f6b47831bf55d49f
jaboll-ai <spexe0011@gmail.com>
2025-12-03 10:54:38 +0100

    init

---
+++ octoprint_clothopus/static/js/clothopus.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-ClothoPus
+ *
+ * Author: Jariem
+ * License: MPL-2.0
+ */
+$(function() {
+    function ClothopusViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/main/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: ClothopusViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_clothopus, #tab_plugin_clothopus, ...
+        elements: [ /* ... */ ]
+    });
+});
---
+++ setup.py
@@ -0,0 +1,4 @@
+import setuptools
+
+# we define the license string like this to be backwards compatible to setuptools<77
+setuptools.setup(license="MPL-2.0")
